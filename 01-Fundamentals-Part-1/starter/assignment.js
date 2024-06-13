e65e89edc752b4dfc62eb8f06dc32bf89b9fd063
pouryourlove <lqu134@naver.com>
2024-06-13 23:20:09 +0900

    value,variable,datatype

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,17 @@
+//Q.values and variables
+let country = "Korea";
+let continent = "Asia";
+let population = "50milion";
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+//Q.data types
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,59 @@
+/*
+let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+//value: piece of data
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+let first = "jonas";
+let firstNamePerson;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+*/
+
+// value is either object or primitive value
+//primitive data
+//number,string,boolean
+//undefined(value taken by a varaible that is not yet defined)
+//null(means empty value)
+//symbol(value that is unique and cannot be changed)
+//Bingint(larger integers than the number type can hold)
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "jonas");
+
+//dynamic typing
+javascriptIsFun = "YES"; //change the value true to "YES"
+//undefined = empty value but decalred
+let year;
+console.log(year); //undefined
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); //object. error in js
