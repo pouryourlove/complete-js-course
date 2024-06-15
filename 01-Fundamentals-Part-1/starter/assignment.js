f61f0f771ab24a1d82b1229fa188787c10af66ce
pouryourlove <lqu134@naver.com>
2024-06-15 19:29:14 +0900

    operators and its assignments

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,17 +1,33 @@
-//Q.values and variables
-let country = "Korea";
-let continent = "Asia";
-let population = "50milion";
+// Q.values and variables
+const  country = "Korea";
+const  continent = "Asia";
+let population = 5 ;
 
 // console.log(country);
 // console.log(continent);
 // console.log(population);
 
 //Q.data types
-let isIsland = false;
+const isIsland = false;
 let language;
 
+//Q,let,const and var
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+language = 'Korean'
+
+const half = population/2;
+console.log(half);
+
+population++;
+
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+const description = country + ' is in ' + continent + ' and its ' + population + ' milion people speak' + language
+console.log(description)
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -62,19 +62,61 @@ console.log(typeof null); //object. error in js
 
 //let: that can change later
 
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-//const: not change at any point
+// //const: not change at any point
 
-const birthYear = 1991;
-// birthYear = 1990; //doesn't work
+// const birthYear = 1991;
+// // birthYear = 1990; //doesn't work
 
-// const job; //doesn't work
-// Use const by default and let if value needs to be changed at some point
+// // const job; //doesn't work
+// // Use const by default and let if value needs to be changed at some point
 
-var job = "programmer";
-job = "teacher"; //mutate is available
+// var job = "programmer";
+// job = "teacher"; //mutate is available
 
-lastName = "Lee"; //it works but always write variable
-console.log(lastName);
+// lastName = "Lee"; //it works but always write variable
+// console.log(lastName);
+//Math operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// // 2 **3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = "juyeon";
+// const lastName = "lee";
+// console.log(firstName + " " + lastName);
+
+// //Assingment operators
+// let x = 10 + 5; //15
+// x += 10; // x = x+ 10 = 25
+// x *= 4; // x = x*4 = 100
+// x++; // x = x + 1
+// x--;
+// x--;
+// console.log(x);
+
+// // Comparison operators
+// console.log(ageJonas > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x,y;
+x = y = 25-10-5; // x = y = 10, x= 10
+console.log(x,y);
+
+const averageAge = (ageJonas + ageSarah)/2;
+console.log(ageJonas,ageSarah, averageAge);
