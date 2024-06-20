ab465584de52d7633d28136a1fcf65e51b5bf84c
pouryourlove <lqu134@naver.com>
2024-06-20 22:16:41 +0900

    object methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -185,21 +185,21 @@
 //property : value
 //order doens't matter unlike arrays
 
-const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Lee',
-    age: 2037 - 1991,
-    job: 'teacher',
-    friends: ['Yuuka', 'Yurika', 'Yoko']
-};
-console.log(jonas);
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Lee',
+//     age: 2037 - 1991,
+//     job: 'teacher',
+//     friends: ['Yuuka', 'Yurika', 'Yoko']
+// };
+// console.log(jonas);
 
-console.log(jonas.lastName);
-console.log(jonas['lastName']);
+// console.log(jonas.lastName);
+// console.log(jonas['lastName']);
 
-const nameKey = 'Name';
-console.log(jonas['first'+ nameKey]);
-console.log(jonas['last' + nameKey]);
+// const nameKey = 'Name';
+// console.log(jonas['first'+ nameKey]);
+// console.log(jonas['last' + nameKey]);
 
 //console.log(jonas.'last'+nameKey)
 //when we need to first compute the property name,
@@ -213,11 +213,49 @@ console.log(jonas['last' + nameKey]);
 //     console.log(`wrong request!`)
 // }
 
-jonas.location = 'Portugal';
-jonas['twitter'] = '@blah';
-console.log(jonas);
+// jonas.location = 'Portugal';
+// jonas['twitter'] = '@blah';
+// console.log(jonas);
 
 //challenge
 //"jonas has 3 friends, and his best friend is called michael"
 
-console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
+// console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
+
+
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Lee",
+  birthyear: 1991,
+  job: "teacher",
+  friends: ["Yuuka", "Yurika", "Yoko"],
+  hasDriversLicense: true,
+  // calcAge: function (birthYear) {
+  //     return 2037 - birthYear
+  // }
+  // calcAge: function () {
+  //     // console.log(this);
+  //     return 2037 - this.birthyear;
+  // }
+  calcAge: function () {
+      this.age = 2037 - this.birthyear;
+      return this.age;
+    },
+    getSummary: function () {
+      return `${this.firstName} is a ${this.calcAge()}-years old ${
+        this.job
+      }, and he ${
+        this.hasDriversLicense ? "has a" : "doesn't have a"
+      } driver's license`;
+  }
+};
+
+console.log(jonas.calcAge());
+console.log(jonas.age);
+// console.log(jonas['calcAge'](1991));
+
+// challenge
+//"Jonas is a 46-year old teacher, and he has a driver's licese."
+
+console.log(jonas.getSummary())
