3a332e0934a21c746b2dcbdac56826075e1dbd83
pouryourlove <lqu134@naver.com>
2024-06-25 23:23:52 +0900

    DOM and DOM manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+//dom - document object model
+//allows javascript to acces html elements and style to manipulate them.
+//connection point between html document and javascript
+//it's stored in the tree stucture
+//document is a special object we can access with javascript
+//it's the entry point to the DOM
+//dom is not part of javascript langauge
+//dom and dom methods are part of web apis that can interact with js
+
+document.querySelector('.message').textContent = 'Correct  Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+//for input value we use value property
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
