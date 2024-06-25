b6f5878a8bbb1a96e83c220cd16f0460cb610f52
pouryourlove <lqu134@naver.com>
2024-06-25 23:52:05 +0900

    EventListener

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
 //dom - document object model
 //allows javascript to acces html elements and style to manipulate them.
@@ -11,10 +11,19 @@ console.log(document.querySelector('.message').textContent);
 //dom is not part of javascript langauge
 //dom and dom methods are part of web apis that can interact with js
 
-document.querySelector('.message').textContent = 'Correct  Number!';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-//for input value we use value property
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.message').textContent = 'Correct  Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// //for input value we use value property
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
 
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess); // it was string before we changed it as Number
+    
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No number'
+    }
+    
+})
