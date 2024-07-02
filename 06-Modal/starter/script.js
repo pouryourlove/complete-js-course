ea9dd39cd5a21f3d3602cd01e4c650938149e08d
pouryourlove <lqu134@naver.com>
2024-07-03 02:04:38 +0900

    modal started

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal); // node
+//querySelector: when there are multiple elements that match,
+//only the first one will be picked
+
+for (let i = 0; i < btnsOpenModal.length; i++){
+    console.log(btnCloseModal[i].textContent);
+}
+
