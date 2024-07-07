3a0fc73649e8ee4c7b3e721781df752c83b301b3
pouryourlove <lqu134@naver.com>
2024-07-08 01:10:55 +0900

    modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -8,7 +8,42 @@ console.log(btnsOpenModal); // node
 //querySelector: when there are multiple elements that match,
 //only the first one will be picked
 
-for (let i = 0; i < btnsOpenModal.length; i++){
-    console.log(btnCloseModal[i].textContent);
+const openModal = function(){
+ 
+        console.log('Button clicked');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden')
+    
 }
 
+const closeModal = function (){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden')  
+}
+
+
+
+for (let i = 0; i < btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click',openModal)
+}
+
+
+btnCloseModal.addEventListener('click',closeModal)
+
+overlay.addEventListener('click',closeModal)
+
+document.addEventListener('keydown',function(e){
+    console.log(e.key)
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal()
+    }
+})
+
+// document.addEventListener('keydown',function(e){
+//     console.log(e.key)
+//     if(e.key === 'Escape'){
+//         if(!modal.classList.contains('hidden')){
+//             closeModal();
+//         }
+//     }
+// })
