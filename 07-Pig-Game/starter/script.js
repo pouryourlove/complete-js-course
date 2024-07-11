00119525fee4517377af0c82aba1a87ee335d482
pouryourlove <lqu134@naver.com>
2024-07-11 23:04:28 +0900

    resetting the game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -6,7 +6,7 @@ const player1El = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
-const current1E1 = document.getElementById('current--1')
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -17,10 +17,28 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0,0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+
+const init = function () {
+  
+ scores = [0, 0];
+ currentScore = 0;
+ activePlayer = 0;
+ playing = true;
+  
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player-active');
+};
+
+init()
 
 const switchPlayer = function () {
       document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -77,6 +95,8 @@ btnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
+
+  btnNew.addEventListener('click', init);
  
 
 })
