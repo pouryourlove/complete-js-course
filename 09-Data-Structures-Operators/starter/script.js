87daf96174c57793a801ac22050d710ed80f2c22
pouryourlove <lqu134@naver.com>
2024-08-02 05:54:59 +0900

    Logical Assignment Operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -438,7 +438,15 @@ console.log(undefined|| null)//null
 console.log(undefined || 0 || '' || 'hello'||null)//hello
 //first truthy vlaue
 
-// restaurant.numGuests = 23;
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log('0일때>>>>>',guests)
+
+//Nullish: null and undefined (not 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log('??>>>>',guestCorrect)
+
+
 const guests1 = restaurant.numGuests? restaurant.numGuests : 10;
 console.log(guests1)//10 
 
@@ -464,3 +472,32 @@ if(restaurant.orderPizza){
 
 restaurant.orderPizza && restaurant.orderPizza('mushrooms','spinach')
 
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi'
+}
+
+//OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// rest2.owner = rest1.owner && '<ANONYMOUS>'; //undefined
+// rest2.owner = rest2.owner && '<ANONYMOUS>';//<ANONYMOUS>
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>'
+
+console.log(rest1) //{name: 'Capri', numGuests: 20}
+console.log(rest2) //{name: 'La Piazza', owner: 'Giovanni Rossi', numGuests: 10}
