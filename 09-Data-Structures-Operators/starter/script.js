02dd8898103abd3a11101ae2e0b35245e76fad32
pouryourlove <lqu134@naver.com>
2024-08-03 10:55:37 +0900

    Maps:iteration

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -839,4 +839,36 @@ console.log(rest.size)
 
 console.log(rest.get(arr)) //undeifned it's not the same object in the heap
 
+const question = new Map([
+  ['question','What is the best programming language in the world?'],
+  [1,'C'],
+  [2,'Java'],
+  [3,'Javascript'],
+  ['correct',3],
+  [true, 'Correct'],
+  [false,'Try again!']
+])
 
+console.log(question)
+
+//Convert Object to map
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap)
+
+//Quiz app
+
+console.log(question.get('question'))
+for(const [key,value] of question){
+  if(typeof key === 'number') console.log(`Answer ${key}: ${value}`)
+}
+
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer))
+
+// Convert map to array
+console.log(([...question]))
+console.log(question.entries())
+console.log(...question.keys())
+console.log(...question.values())
